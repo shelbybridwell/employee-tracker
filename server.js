7a1a9c45d509e9d084868064d13f29471250fb85
Shelby Bridwell <109699848+shelbybridwell@users.noreply.github.com>
2023-01-17 22:16:05 -0600

    first prompts

--- server.js
+++ server.js
@@ -12,3 +12,59 @@ const connnection = mysql.createConnection({
     password: ''
     database: 'employeeDB'
 });
+
+connection.connect(function (err) {
+    if (err) throw err;
+    console.log("connected as id " + connection.threadId);
+    console.log(`employee manager`)
+    // runs the app
+    firstPrompt();
+});
+function firstPrompt() {
+
+    inquirer
+        .prompt({
+            type: "list",
+            name: "task",
+            message: "Make your selection...",
+            choices: [
+                "View Employees",
+                "View Employees by Department",
+                "Add Employee",
+                "Remove Employee",
+                "Update Employee Role",
+                "Add Role",
+                "End"]
+        })
+        .then(function ({ task })) {
+            switch (task) {
+                case "View Employees":
+                    viewEmployee();
+                    break;
+          
+                  case "View Employees by Department":
+                    viewEmployeeByDepartment();
+                    break;
+                
+                  case "Add Employee":
+                    addEmployee();
+                    break;
+          
+                  case "Remove Employees":
+                    removeEmployees();
+                    break;
+          
+                  case "Update Employee Role":
+                    updateEmployeeRole();
+                    break;
+          
+                  case "Add Role":
+                    addRole();
+                    break;
+          
+                  case "End":
+                    connection.end();
+                    break;
+            }
+        });
+}
